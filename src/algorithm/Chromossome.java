61894f2770c13604eda8412504b0b7ba70177168
aslmbc13 <asl_mbc_13@hotmail.com>
2016-04-26 19:12:40 +0100

    ...

--- src/algorithm/Chromossome.java
+++ src/algorithm/Chromossome.java
@@ -4,20 +4,17 @@ public abstract class Chromossome {
 	
 	public abstract String toString();
 	
-	public static String[] crossover(Chromossome c1, Chromossome c2, int split_index) {
+	public static String[] crossover(String c1, String c2, int split_index) {
 		String[] result = new String[2];
 		
-		String c1_content = c1.toString();
-		String c2_content = c2.toString();
-		
-		if(c1_content.length() != c2_content.length() || split_index > c1_content.length()-1)
+		if(c1.length() != c2.length() || split_index > c1.length()-1)
 			return null;
 		
-		int length = c1_content.length();
-		String new_c1 = "" + c1_content.substring(0, split_index);
-		String new_c2 = "" + c2_content.substring(0, split_index);
-		new_c1 += c2_content.substring(split_index, length);
-		new_c2 += c1_content.substring(split_index, length);
+		int length = c1.length();
+		String new_c1 = "" + c1.substring(0, split_index);
+		String new_c2 = "" + c2.substring(0, split_index);
+		new_c1 += c2.substring(split_index, length);
+		new_c2 += c1.substring(split_index, length);
 		
 		result[0] = new_c1;
 		result[1] = new_c2;
